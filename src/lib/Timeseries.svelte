0ef387b8bc3a866a5b3ce4338828d0977abdf556
esf <efrain.sotelo.ferry@gmail.com>
2022-02-26 21:38:51 +0100

    feat: zoom by click zone , pan with shift and autocolors

--- src/lib/Timeseries.svelte
+++ src/lib/Timeseries.svelte
@@ -11,10 +11,11 @@
 		Legend,
 		type ChartOptions
 	} from 'chart.js';
-	import '@egjs/hammerjs';
+	import autocolors from 'chartjs-plugin-autocolors';
 	import zoomPlugin from 'chartjs-plugin-zoom';
 	import { onMount } from 'svelte';
 
+	Chart.register(autocolors);
 	Chart.register(zoomPlugin);
 	Chart.register(
 		LineElement,
@@ -30,24 +31,41 @@
 	let title = 'test';
 	let chartElem: HTMLCanvasElement;
 	let chart: Chart;
+	let zoomOptions = {
+		pan: {
+			enabled: true,
+			mode: 'xy'
+		},
+		zoom: {
+			wheel: {
+				enabled: true
+			},
+			pinch: {
+				enabled: true
+			},
+			mode: 'xy'
+		}
+	};
+	zoomOptions = {
+		pan: {
+			enabled: true,
+			mode: 'xy',
+			modifierKey: 'shift'
+		},
+		zoom: {
+			mode: 'xy',
+			drag: {
+				enabled: true,
+				borderColor: 'rgb(54, 162, 235)',
+				borderWidth: 1,
+				backgroundColor: 'rgba(54, 162, 235, 0.3)'
+			}
+		}
+	};
 	let options: ChartOptions = {
 		responsive: true,
 		plugins: {
-			zoom: {
-				pan: {
-					enabled: true,
-					mode: 'xy'
-				},
-				zoom: {
-					wheel: {
-						enabled: true
-					},
-					pinch: {
-						enabled: true
-					},
-					mode: 'xy'
-				}
-			},
+			zoom: zoomOptions,
 			title: {
 				display: true,
 				text: title,
@@ -55,7 +73,8 @@
 					size: 18,
 					weight: 'bold'
 				}
-			}
+			},
+			autocolors
 		},
 		scales: {
 			x: {
@@ -84,8 +103,8 @@
 			data: {
 				labels: ['0', '1', '2', '3'],
 				datasets: [
-					{ label: 'test1', data: [20, 10, 30, 40], fill: false, borderColor: 'rgb(75, 192, 192)' },
-					{ label: 'test2', data: [10, 5, 15, 20], fill: false, borderColor: 'rgb(75, 192, 192)' }
+					{ label: 'test1', data: [20, 10, 30, 40], fill: false },
+					{ label: 'test2', data: [10, 5, 15, 20], fill: false }
 				]
 			},
 			options
